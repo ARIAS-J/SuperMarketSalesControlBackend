20ea723e83da775a2f3e4c99d5b41fa7f5ced79d
No-Hex <evilheroe1@gmail.com>
2023-03-11 23:37:47 -0400

    initial commit

---
+++ src/main/java/com/NoHex/SuperMarketSalesControlBackend/SuperMarketSalesControlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.NoHex.SuperMarketSalesControlBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperMarketSalesControlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperMarketSalesControlBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NoHex/SuperMarketSalesControlBackend/SuperMarketSalesControlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NoHex.SuperMarketSalesControlBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperMarketSalesControlBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
